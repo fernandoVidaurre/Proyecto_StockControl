7a76eef3ec4c7aefa8930aa59aabe0655e6e8ce5
= <juliofernandovidaurre@gmail.com>
2023-05-15 11:09:42 -0300

    creacion de modelos y migracion

--- StockControl/compra/models.py
+++ StockControl/compra/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Proveedor(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    dni =models.IntegerField
+
+    def __str__(self):
+         
+        return f" {self.nombre} - {self.apellido} - {self.dni}"
+class Producto(models.Model):
+    nombre = models.CharField(max_length=25)
+    precio = models.FloatField(default=0)
+    stock_actual = models.IntegerField(default=0)
+    proveedor = models.ForeignKey(
+        Proveedor,
+        related_name="productos",
+        on_delete=models.CASCADE
+    )
+    def __str__(self):
+        return f" {self.nombre} - {self.precio} - {self.stock_actual} - {self.proveedor}"
